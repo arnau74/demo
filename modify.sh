d664f023341ae1e7978ced9d0c18d266331fdc2f
Ubuntu <tonino@terraform.zvw0asvdqzfevdggyvhdyrsy4c.ax.internal.cloudapp.net>
2020-02-11 14:39:54 +0000

    hello

--- modify.sh
+++ modify.sh
@@ -1,1 +1,3 @@
 cp /home/tonino/NodeInfoServlet.java src/main/java/com/dxc/ecaas/demo/
+
+cp /home/tonino/wildfly-banner-1180px.png.t8 /home/tonino/git/demo/src/main/webapp/images/wildfly-banner-1180px.png
