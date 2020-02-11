4c49028980fceda05073f6961854fc05394210e6
Ubuntu <tonino@terraform.zvw0asvdqzfevdggyvhdyrsy4c.ax.internal.cloudapp.net>
2020-02-11 14:45:21 +0000

    hello

--- src/main/java/com/dxc/ecaas/demo/NodeInfoServlet.java
+++ src/main/java/com/dxc/ecaas/demo/NodeInfoServlet.java
@@ -29,7 +29,7 @@ public class NodeInfoServlet extends HttpServlet {
 	writer.println("<hr/>");
 
 	/* Put your content here */
-	writer.println("<h1>Hello from the SE Core Delivery Skills and Development Plan Team 8 rel 3!</h1>");
+	writer.println("<h1>Work in progress!!</h1>");
 	/* HTML trailer */
 	writer.println("</body>\n</html>");
 
